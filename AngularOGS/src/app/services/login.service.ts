922b20abf9b73544eb814e54ddb974c966d7cef3
Andrew Hwang <ahwang1995@gmail.com>
2018-07-26 14:00:22 -0500

    setup login

---
+++ AngularOGS/src/app/services/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './../objects/user';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  checkLogin(){
+    this.http.post("endpoint_url", {
+      key1: "value",
+      key2: "value",
+      etc: "value"
+      }).subscribe(
+        PASS => {},
+        FAIL => {})
+  }
+}

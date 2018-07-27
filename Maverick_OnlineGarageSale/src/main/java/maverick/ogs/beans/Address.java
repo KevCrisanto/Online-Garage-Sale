f60c63afcb01735f826eed0de06629e92f180679
nisfornature <etaoinshrdluwho@gmail.com>
2018-07-27 14:33:35 -0500

    added to beans

--- Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/Address.java
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/Address.java
@@ -1,5 +1,13 @@
 package maverick.ogs.beans;
 
 public class Address {
-
+	private String addressId;
+	private String addressLine1;
+	private String addressLine2;
+	private String AptNumber;
+	private String postOfficeBox;
+	private String city;
+	private String state;
+	private String country;
+	private Integer zipcode;
 }
---
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/Item.java
@@ -0,0 +1,15 @@
+package maverick.ogs.beans;
+
+public class Item {
+	private String itemId;
+	
+	private String userId;
+	
+	private String description;
+	
+	private String itemStatus;
+	
+	private float price;
+	
+	private String category;
+}

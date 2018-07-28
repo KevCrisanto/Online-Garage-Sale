29927350102cfabcb268782e240f8d20d51ab05a
nisfornature <etaoinshrdluwho@gmail.com>
2018-07-28 09:11:28 -0500

    Added Item, File, and CreditCard beans to hibernate.cfg.xml as mappings

--- Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/File.java
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/File.java
@@ -1,7 +1,73 @@
 package maverick.ogs.beans;
 
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.sql.rowset.serial.SerialException;
+
 public class File {
+	@Id
+	@Column(name = "file_id")
 	private String fileId;
+	
+	@OneToOne
+	@JoinColumn(name = "item_id")
 	private String itemId;
-	private Byte[] data;
+	
+	@Column(name = "file")
+	private Blob file;
+	
+	public File() {
+		super();
+		this.fileId = UUID.randomUUID().toString();
+	}
+	
+	public File(String itemId, Blob file) {
+		this.fileId = UUID.randomUUID().toString();
+		this.itemId = itemId;
+		this.file = file;
+	}
+	
+	public File(String itemId, byte[] file) {
+		try {
+			this.fileId = UUID.randomUUID().toString();
+			this.itemId = itemId;
+			this.file = new javax.sql.rowset.serial.SerialBlob(file);
+		} catch (SerialException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public Blob getFile() {
+		return file;
+	}
+
+	public void setFile(Blob file) {
+		this.file = file;
+	}
+	
+	
 }
--- Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/Item.java
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/beans/Item.java
@@ -1,15 +1,101 @@
 package maverick.ogs.beans;
 
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Item {
+	
+	@Id
+	@Column(name = "item_id")
 	private String itemId;
 	
-	private String userId;
+	@ManyToOne
+	@JoinColumn(name = "account_id")
+	private String accountId;
 	
+	@Column(name = "description")
 	private String description;
 	
+	@Column(name = "item_status")
 	private String itemStatus;
 	
+	@Column(name = "price")
 	private float price;
 	
+	@Column(name = "category")
 	private String category;
+	
+	public Item() {
+		this.itemId = UUID.randomUUID().toString();
+	}
+	
+	public Item(String itemId, String accountId, String description, String itemStatus, float price, String category) {
+		this.itemId = itemId;
+		this.accountId = accountId;
+		this.description = description;
+		this.itemStatus = itemStatus;
+		this.price = price;
+		this.category = category;
+	}
+	
+	public Item(String accountId, String description, String itemStatus, float price, String category) {
+		this.itemId = UUID.randomUUID().toString();
+		this.accountId = accountId;
+		this.description = description;
+		this.itemStatus = itemStatus;
+		this.price = price;
+		this.category = category;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getItemStatus() {
+		return itemStatus;
+	}
+
+	public void setItemStatus(String itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }

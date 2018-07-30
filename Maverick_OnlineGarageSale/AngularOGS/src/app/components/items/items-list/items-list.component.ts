b7120b0561e0bc41a5eb8d8cdead6e7dbd9fdf5d
Kevin Crisanto <kevincrisanto@gmail.com>
2018-07-30 15:39:31 -0500

    Hardcoded item list design

--- Maverick_OnlineGarageSale/AngularOGS/src/app/components/items/items-list/items-list.component.ts
+++ Maverick_OnlineGarageSale/AngularOGS/src/app/components/items/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../../objects/item';
 
 @Component({
   selector: 'app-items-list',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-list.component.css']
 })
 export class ItemsListComponent implements OnInit {
+  items: Item[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.items = [
+      {
+        id: 1,
+        name: 'Ferrari',
+        description: 'it is a car',
+        category: 'cars',
+        itemStatus: 'sold',
+        price: 180000,
+        accountId: 1
+      },
+      {
+        id: 2,
+        name: 'Ferrari 2',
+        description: 'it is a car 2',
+        category: 'cars 2',
+        itemStatus: 'sold',
+        price: 180000,
+        accountId: 1
+      }
+    ];
   }
 
+  ngOnInit() {}
 }
--- Maverick_OnlineGarageSale/AngularOGS/src/app/objects/item.ts
+++ Maverick_OnlineGarageSale/AngularOGS/src/app/objects/item.ts
@@ -1,6 +1,6 @@
 export class Item {
   constructor(
-    public id: string,
+    public id: number,
     public name: string,
     public description: string,
     public category: string,

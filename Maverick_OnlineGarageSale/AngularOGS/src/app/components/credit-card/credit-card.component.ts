1418535e9261e2bbbcc8e339c920920dcc52cb4d
Kevin Crisanto <kevincrisanto@gmail.com>
2018-07-31 15:59:53 -0500

    Hardcoded item-details

--- Maverick_OnlineGarageSale/AngularOGS/src/app/components/credit-card/credit-card.component.ts
+++ Maverick_OnlineGarageSale/AngularOGS/src/app/components/credit-card/credit-card.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef, DoCheck } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  DoCheck
+} from '@angular/core';
 import { CardModule } from 'ngx-card/ngx-card';
 import { NgForm } from '../../../../node_modules/@angular/forms';
 import { Account } from '../../objects/account';
@@ -12,24 +18,36 @@ import { CardService } from '../../services/card.service';
   styleUrls: ['./credit-card.component.css']
 })
 export class CreditCardComponent implements OnInit, DoCheck {
-
-  imports: [
-    CardModule
-  ];
+  imports: [CardModule];
 
   @ViewChild('cardNumber') cardNumber: ElementRef;
 
-  constructor(private cardS: CardService) { }
+  constructor(private cardS: CardService) {}
   invalid;
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  account = new Account('', '', '', '', '', '', null, false, false, false, false);
+  account = new Account(
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    null,
+    false,
+    false,
+    false,
+    false,
+    null
+  );
   // address = new Address('','','','','','','','','');
-  creditCard = new Card('',this.account, '', '','',null)
+  creditCard = new Card('', this.account, '', '', '', null);
 
   ngDoCheck() {
-    if (this.cardNumber.nativeElement.classList.contains('jp-card-invalid') || this.cardNumber.nativeElement.value === '') {
+    if (
+      this.cardNumber.nativeElement.classList.contains('jp-card-invalid') ||
+      this.cardNumber.nativeElement.value === ''
+    ) {
       console.log('Error');
       this.invalid = true;
     } else {
@@ -37,8 +55,7 @@ export class CreditCardComponent implements OnInit, DoCheck {
     }
   }
 
-  insert(c: Card){
+  insert(c: Card) {
     this.cardS.cardService(this.creditCard).subscribe();
   }
-
 }

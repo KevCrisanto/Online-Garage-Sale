caa6cbc46a790028b8df5669449d613aff6f5cf4
nisfornature <etaoinshrdluwho@gmail.com>
2018-08-01 17:03:51 -0500

    added selenium Utils class for misc. selenium utilities

---
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/selenium/Utils.java
@@ -0,0 +1,20 @@
+package maverick.ogs.selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Utils {
+	private static int millisecondsToWait = 800;
+	
+	public static void emulateUserKeyStroke(WebDriver driver, String input, By element) {
+		for (Character c : input.toCharArray()) {
+			try {
+				Thread.sleep(millisecondsToWait);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			driver.findElement(element).sendKeys(String.valueOf(c));
+		}
+	}
+}

7efa772266a27620611a7f91058fa311edb7f4ab
nisfornature <etaoinshrdluwho@gmail.com>
2018-07-31 23:16:34 -0500

    Added seleniumtest along with drivers supporting test

---
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/selenium/pages/Login.java
@@ -0,0 +1,43 @@
+package maverick.ogs.selenium.pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Login {
+	private WebDriver driver;
+	private WebDriverWait explicitWait;
+	private By username = By.xpath("//input[@name='username']");
+	private By password = By.xpath("//input[@name='password']");
+	private By rememberMe = By.xpath("//input[@name='login']");
+	private By submit = By.cssSelector("input[type='text']");
+	
+	public Login(WebDriver driver) {
+		this.driver = driver;
+		explicitWait = new WebDriverWait(driver, 5);
+	}
+	
+	public void inputUsername(String username){
+		driver.findElement(this.username).sendKeys(username);
+	}
+	public String getUsername(){
+		return driver.findElement(this.username).getText();
+	}
+	public void inputPassword(String password){
+		driver.findElement(this.password).sendKeys(password);
+	}
+	public void clickRememberMe() {
+		driver.findElement(rememberMe).click();
+	}
+	public void submitLogic(){
+		driver.findElement(submit).click();
+	}
+	public void loginToOGS(String username, String password){
+		inputUsername(username);
+		
+		inputPassword(password);
+		submitLogic();
+	}
+}

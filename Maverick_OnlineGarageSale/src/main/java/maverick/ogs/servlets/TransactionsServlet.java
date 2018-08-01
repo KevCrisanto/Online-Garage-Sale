54796b96ed8e49699aff41945be164061a87aa39
nisfornature <etaoinshrdluwho@gmail.com>
2018-07-31 20:52:36 -0500

    Added TransactionsServlet

--- Maverick_OnlineGarageSale/src/main/java/maverick/ogs/servlets/TransactionsServlet.java
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/servlets/TransactionsServlet.java
@@ -1,9 +1,19 @@
 package maverick.ogs.servlets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import maverick.ogs.beans.Transactions;
 import maverick.ogs.dao.TransactionsDAO;
 import maverick.ogs.dao.TransactionsDAOImpl;
 
@@ -14,12 +24,17 @@ public class TransactionsServlet extends HttpServlet {
 		
 	}
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse respone) {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		TransactionsDAO transactionsDAO = new TransactionsDAOImpl();
+		List<Transactions> transactions = transactionsDAO.getAllTransactions();
+		// Gson gson = new GsonBuilder().create();
 		
+		String json = new Gson().toJson(transactions);
+		response.setContentType("application/json");
+		response.getWriter().write(json);
 	}
 }

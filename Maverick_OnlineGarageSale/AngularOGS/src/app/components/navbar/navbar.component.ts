1483e2fcd8fc25ad9c912e1a37fc2febf06119be
Vladimir B <vladimir.bukhalo@gmail.com>
2018-08-02 18:42:18 -0500

    logout stuff

--- Maverick_OnlineGarageSale/AngularOGS/src/app/components/navbar/navbar.component.ts
+++ Maverick_OnlineGarageSale/AngularOGS/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../../services/navbar.service';
+import { LoginService } from '../../services/login.service';
+import { Account } from './../../objects/account';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +11,17 @@ import { NavbarService } from '../../services/navbar.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private nav: NavbarService) { }
+  ResetAcc = new Account('','', '', '', '', '', null, false, false, false, false,null);
+
+  constructor(private nav: NavbarService, private login: LoginService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.login.changeAccount(this.ResetAcc);
+    this.router.navigate(['login']);
+    
   }
 
 

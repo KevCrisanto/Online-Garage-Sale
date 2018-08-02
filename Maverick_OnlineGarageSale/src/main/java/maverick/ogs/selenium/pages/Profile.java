7046e3a6efdc9baf844e051998758c7784d90795
nisfornature <etaoinshrdluwho@gmail.com>
2018-08-02 11:50:57 -0500

    Added Selenium tests.

--- Maverick_OnlineGarageSale/src/main/java/maverick/ogs/selenium/pages/Profile.java
+++ Maverick_OnlineGarageSale/src/main/java/maverick/ogs/selenium/pages/Profile.java
@@ -8,14 +8,18 @@ import maverick.ogs.selenium.Utils;
 
 public class Profile {
 	private WebDriver driver;
-	private By firstName = By.xpath("//*[@id=\"staticfname\"]");
-	private By lastName = By.xpath("//*[@id=\"staticlname\"]");
+	private static final String url = "http://18.219.13.188:8085/Maverick_OnlineGarageSale/AngularOGS/#/profile";
+	private By firstName = By.xpath("//*[@id=\"fname\"]");
+	private By lastName = By.xpath("//*[@id=\"lname\"]");
+	private By editClickable = By.xpath("/html/body/app-root/div/app-profile/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[2]/fa-icon");
 	
 	public Profile(WebDriver driver) {
 		this.driver = driver;
+		driver.get(url);
 	}
 	
 	public void inputFirstName(String firstName) {
+		Utils.clearField(this.firstName, driver);
 		Utils.emulateUserKeyStroke(driver, firstName, this.firstName);
 		
 		try {
@@ -26,6 +30,7 @@ public class Profile {
 	}
 	
 	public void inputLastName(String lastName) {
+		Utils.clearField(this.lastName, driver);
 		Utils.emulateUserKeyStroke(driver, lastName, this.lastName);
 		try {
 			Thread.sleep(3000);
@@ -34,7 +39,11 @@ public class Profile {
 		}
 	}
 	
+	public void clickEditIcon() {
+		driver.findElement(editClickable).click();
+	}
 	public void testProfile(String firstName, String lastName) {
+		clickEditIcon();
 		inputFirstName(firstName);
 		inputLastName(lastName);
 	}

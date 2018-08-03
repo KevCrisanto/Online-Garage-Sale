e18566d7d43767f49c70cddcaf6a46443b2d118e
Vladimir B <vladimir.bukhalo@gmail.com>
2018-08-02 19:10:24 -0500

    logout button

--- Maverick_OnlineGarageSale/AngularOGS/src/app/components/navbar/navbar.component.ts
+++ Maverick_OnlineGarageSale/AngularOGS/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { NavbarService } from '../../services/navbar.service';
 import { LoginService } from '../../services/login.service';
 import { Account } from './../../objects/account';
 import { Router } from '@angular/router';
+import { CookieService } from '../../../../node_modules/ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,14 +14,17 @@ export class NavbarComponent implements OnInit {
 
   ResetAcc = new Account('','', '', '', '', '', null, false, false, false, false,null);
 
-  constructor(private nav: NavbarService, private login: LoginService, private router: Router) { }
+  constructor(private nav: NavbarService, private login: LoginService, private router: Router, private cookie: CookieService) { }
 
   ngOnInit() {
   }
 
   logout(){
-    this.login.changeAccount(this.ResetAcc);
-    this.router.navigate(['login']);
+    this.cookie.delete('userid'); 
+    // setTimeout(() => this.router.navigate(['login']), 5000);
+    location.reload();
+    this.router.navigate(['login'])
+    
     
   }
 

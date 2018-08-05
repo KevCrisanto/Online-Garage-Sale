2964938d697f3c782afbdc7efbc128bc404ed740
nisfornature <etaoinshrdluwho@gmail.com>
2018-08-05 14:45:32 -0500

    Added Selenium tests with test config options to run tests on a local Project 2.

--- Maverick_OnlineGarageSale/src/main/test/maverick/ogs/junit/OnlineGarageSaleLoginTest.java
+++ Maverick_OnlineGarageSale/src/main/test/maverick/ogs/junit/OnlineGarageSaleLoginTest.java
@@ -1,5 +1,10 @@
 package maverick.ogs.junit;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -13,12 +18,34 @@ import maverick.ogs.selenium.pages.Profile;
 
 public class OnlineGarageSaleLoginTest {
 	public static WebDriver driver;
-	public final String url = "http://18.219.13.188:8085/Maverick_OnlineGarageSale/AngularOGS/";
+	public static final String FILE_NAME = "src/main/resources/testConfigs.properties";
+	private final String localhostURL = "http://localhost:8085";
+	private final String ec2URL = "http://18.219.13.188:8085";
+	public String url = "/Maverick_OnlineGarageSale/AngularOGS/";
 	public Login login;
 	public static Profile profile;
+	private static Properties testConfigs = null;
+	private Boolean isLocalConnection = false;
 	
 	@Before
 	public void setup() {
+		try {
+			testConfigs = new Properties();
+			testConfigs.load(new FileInputStream(FILE_NAME));
+			this.isLocalConnection = Boolean.valueOf(testConfigs.getProperty("isLocalConnection"));
+			
+		} catch (FileNotFoundException fnfe) {
+			this.isLocalConnection = false;
+		} catch (IOException ioe) {
+			this.isLocalConnection = false;
+		}
+		
+		if (this.isLocalConnection) {
+			this.url = localhostURL + url;
+		} else {
+			this.url = ec2URL + url;
+		}
+		
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -29,8 +56,6 @@ public class OnlineGarageSaleLoginTest {
 	public void loginTest() {
 		login = new Login(driver);
 		login.loginToOGS("bobbert", "bobbert", "someNewUser", "password", "somePerson@email.com");
-		profile = new Profile(driver);
-		profile.testProfile("Today is my birthday. Not!", "Jones", "Story Rd.", "", "", "2L", "Pallet Town", "United States", "76013");
 	}
 	
 	@After
